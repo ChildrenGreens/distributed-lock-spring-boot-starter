e2d4d7472e71d38b95848d1b1bbc2735025d6535
qiujun_cai <qiujun_cai@meta48.com>
2025-12-23 11:19:31 +0800

    Add license headers and configuration files for project setup

--- src/main/java/com/childrengreens/distributedlock/aop/DistributedLockAdvisor.java
+++ src/main/java/com/childrengreens/distributedlock/aop/DistributedLockAdvisor.java
@@ -1,7 +1,23 @@
+/*
+ * Copyright 2012-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.childrengreens.distributedlock.aop;
 
 import com.childrengreens.distributedlock.annotation.DistributedLock;
 import org.aopalliance.intercept.MethodInterceptor;
+import org.jspecify.annotations.NonNull;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.aop.support.StaticMethodMatcherPointcutAdvisor;
 import org.springframework.core.Ordered;
@@ -19,9 +35,8 @@ public class DistributedLockAdvisor extends StaticMethodMatcherPointcutAdvisor {
     }
 
     @Override
-    public boolean matches(Method method, Class<?> targetClass) {
-        Class<?> userClass = targetClass != null ? targetClass : method.getDeclaringClass();
-        Method specificMethod = AopUtils.getMostSpecificMethod(method, userClass);
+    public boolean matches(@NonNull Method method, @NonNull Class<?> targetClass) {
+        Method specificMethod = AopUtils.getMostSpecificMethod(method, targetClass);
         return AnnotatedElementUtils.hasAnnotation(specificMethod, DistributedLock.class)
                 || AnnotatedElementUtils.hasAnnotation(method, DistributedLock.class);
     }
